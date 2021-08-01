4cf91e0abd54359c1854e950acc99842f543e6f4
atrem13 <mertayoga17.my@gmail.com>
2021-08-01 19:32:35 +0800

    update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
